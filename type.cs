2f6f0e912dade88fc2ed6161f494d56ca4bc232f
clmmbs <msreywel@hotmail.com>
2018-06-12 01:03:23 +0300

    Add files via upload

---
+++ type.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+public class type
+{
+    static void Main(string[] args)
+    {
+        switch (args.Length)
+        {
+            case 1:
+                if (args[0].Contains(";"))
+                {
+                    string[] files = args[0].Split(new char[] { ';' });
+                    foreach (string file in files)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("now typing " + file);
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.Write(File.ReadAllText(file));
+                    }
+                }
+                else
+                {
+                    string content = File.ReadAllText(args[0]);
+                    Console.Write(content);
+                }
+                break;
+            case 0:
+                Console.WriteLine("the syntax is wrong");
+                break;
+            default:
+                foreach (string file in args)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("now typing " + file);
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.Write(File.ReadAllText(file));
+                }
+                break;
+        }
+    }
+}
